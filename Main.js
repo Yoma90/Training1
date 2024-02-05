47533e487f6f8ce39c56a6dc04af111d4ffca2cd
Yoma90 <yomataniel@gmail.com>
2024-02-05 21:45:38 +0100

    setting up the project

---
+++ Main.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Main = () => {
+  return (
+    <View>
+      <Text>The first page of my app</Text>
+    </View>
+  )
+}
+
+export default Main
+
+const styles = StyleSheet.create({})
